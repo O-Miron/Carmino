8079f78f7f89680457066a28bd8f163fc67433c4
O-Miron <82511306@ulife.com.br>
2025-04-17 19:25:48 -0300

    Add files via upload

---
+++ AtividadeCarmino4/src/OperadorTernarioNota.java
@@ -0,0 +1,31 @@
+import javax.swing.JOptionPane;
+public class OperadorTernarioNota {
+    public static void main(String[] args) {
+        
+         int nota1, nota2, nota3;
+         double media;
+        
+        nota1 = Integer.parseInt(JOptionPane.showInputDialog(" Qual a sua primeira nota?"));
+        nota2 = Integer.parseInt(JOptionPane.showInputDialog(" Qual a sua segunda nota?"));
+        nota3 = Integer.parseInt(JOptionPane.showInputDialog(" Qual a sua terceira nota?"));
+        media = (nota1 + nota2+ nota3)/3.0;
+        
+        String situacao = (media>7)? "Aprovado" : (media <6.9 && media >5)? "Recuperação" : "Reprovado"; 
+        
+        if (media > 7){
+            System.out.println("Aprovado");
+        }
+        else if (media <6.9 && media > 5){
+            System.out.println("Recuperacao");
+        }
+        else {
+            System.out.println("");
+        }
+        
+        
+        JOptionPane.showMessageDialog(null, "Sua média é: " + media + ". " + situacao);
+        
+        
+        
+    }
+}
